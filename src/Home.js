f65911019f8cd3fe0409e283a74a7b7f5743c7f7
Manuel Alvarez <manuel_ale94@outlook.com>
2023-06-15 21:14:25 -0600

    FEAT: Se agrega NavBar Template

--- src/Home.js
+++ src/Home.js
@@ -3,15 +3,19 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import './css/App.css';
+import HeaderNav from "./Header";
 
 const Home = () => {
     return (
-        <Container fluid="md">
-            <Row>
-                <Col sm={4}>
-                    <h1 className="text-centered">Home Page</h1>
-                </Col>
-            </Row>
+        <Container fluid>
+            <HeaderNav />
+            <Container  fluid="md">
+                <Row>
+                    <Col sm={4}>
+                        <h1 className="text-centered">Home Page</h1>
+                    </Col>
+                </Row>
+            </Container>
         </Container>
     )
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Home from './Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const container = document.getElementById('root')
 const root = ReactDOM.createRoot(container);
